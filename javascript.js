00e92c8a7fe02d48f26637f1ddf2e198d823c283
alex-wichman <alex.d.wichman@gmail.comgit config --global user.name alex-wichmangit config --global user.name alex-wichmanexitclear>
2024-07-24 15:08:53 -0500

    finished

--- javascript.js
+++ javascript.js
@@ -1,68 +1,11 @@
 
-function playGame(humanChoice, computerChoice) 
-{
-    let humanScore = 0;
-    let computerScore = 0;
-    function playRound(humanChoice, computerChoice) 
-    {
-        humanChoice = humanChoice.toLowerCase();
+let humanScore = 0;
+let computerScore = 0;
+let compChoice;
+let gameCount = 0
 
-        if (humanChoice == "rock" && computerChoice == "paper")
-            {
-                console.log("You Lose! paper beats rock");
-                computerScore++;
-                return;
-
-            }
-        if (humanChoice == "paper" && computerChoice == "rock")
-            {
-                console.log("You Win! paper beats rock");
-                humanScore++;
-                return;
-
-            }
-        if (humanChoice == "scissors" && computerChoice == "paper")
-            {
-                console.log("You Win! scissors beats paper");
-                humanScore++;
-                return;
-
-            }
-        if (humanChoice == "paper" && computerChoice == "scissors")
-            {
-                console.log("You Lose! scissors beats paper");
-                computerScore++;
-                return;
-
-            }
-        if (humanChoice == "rock" && computerChoice == "scissors")
-            {
-                console.log("You Win! rock beats scissors");
-                humanScore++;
-                return;
-
-            }
-        if (humanChoice == "scissors" && computerChoice == "rock")
-            {
-                console.log("You Lose! rock beats scissors");
-                computerScore++;
-                return;
-            }
-        
-        console.log("You tie!");
-        return;
-
-    }
-   
-    for (let i = 0; i < 5;  i++)
-        {
-            computerChoice = getComputerChoice();
-            humanChoice = getHumanChoice();
-            playRound(humanChoice,computerChoice);
-        }
-
-        console.log("final scores: Computer: " + computerScore + " Human: " + humanScore);
-}
+document.getElementById('ys').textContent = humanScore;
+document.getElementById('cs').textContent = computerScore;
 
 function getComputerChoice() 
 {
@@ -85,19 +28,120 @@ function getComputerChoice()
     return computerChoice;
 }
 
-function getHumanChoice()
+
+function playRound() 
 {
-    let getHumanChoice = prompt("Enter your choice: ");
-    return getHumanChoice;
+    let humanChoice = document.getElementById('you').textContent;
+    let computerChoice = document.getElementById('comp').textContent;
+    let message;
+    if (humanChoice == "rock" && computerChoice == "paper")
+        {
+            message = "You Lose! paper beats rock";
+            computerScore++;
+            
+
+        }
+    else if (humanChoice == "paper" && computerChoice == "rock")
+        {
+            message = "You Win! paper beats rock";
+            humanScore++;
+            
+
+        }
+    else if (humanChoice == "scissors" && computerChoice == "paper")
+        {
+            message = "You Win! scissors beats paper";
+            humanScore++;
+            
+
+        }
+    else if (humanChoice == "paper" && computerChoice == "scissors")
+        {
+            message = "You Lose! scissors beats paper";
+            computerScore++;
+            
+
+        }
+    else if (humanChoice == "rock" && computerChoice == "scissors")
+        {
+            message = "You Win! rock beats scissors";
+            humanScore++;
+            
+
+        }
+    else if (humanChoice == "scissors" && computerChoice == "rock")
+        {
+            message = "You Lose! rock beats scissors";
+            computerScore++;
+            
+        }
+    else {
+        message = "You tie!"; }
+    
+    document.getElementById('ys').textContent = humanScore;
+    document.getElementById('cs').textContent = computerScore;
+    gameCount++;
+    document.getElementById('gameCt').textContent = "Games Played: " + gameCount;
+    return message;
 
 }
 
+function printCompChoice(compChoice)
+{
+    if (compChoice == 'rock')
+    {
+        document.getElementById('comp').textContent = 'rock';
+    }
 
+    if (compChoice == 'paper')
+    {
+        document.getElementById('comp').textContent = 'paper';
+    }
+    if (compChoice == 'scissors')
+    {
+        document.getElementById('comp').textContent = 'scissors';
+    }
 
-let computerChoice;
-let humanChoice;
+}
 
+function printMessage(message)
+{
+    document.getElementById('msg').textContent = message;
+}
 
+document.getElementById('Rock').onclick = function(){
+    document.getElementById('you').textContent = "rock";
+    compChoice = getComputerChoice();
+    printCompChoice(compChoice);
+    message = playRound();
+    printMessage(message);
 
-playGame(humanChoice, computerChoice);
+}
 
+document.getElementById('Paper').onclick = function(){
+    document.getElementById('you').textContent = 'paper';
+    compChoice = getComputerChoice();
+    printCompChoice(compChoice);
+    message = playRound();
+    printMessage(message);
+}
+
+document.getElementById('Scissors').onclick = function(){
+    document.getElementById('you').textContent = 'scissors';
+    compChoice = getComputerChoice();
+    printCompChoice(compChoice);
+    message = playRound();
+    printMessage(message);
+}
+   
+document.getElementById('reset').onclick = function(){
+    humanScore = 0;
+    computerScore = 0;
+    document.getElementById('ys').textContent = humanScore;
+    document.getElementById('cs').textContent = computerScore;
+    document.getElementById('msg').textContent = "Let's play rock-paper-scissors!";
+    document.getElementById('comp').textContent = '';
+    document.getElementById('you').textContent = '';
+    document.getElementById('gameCt').textContent = "Games Played: 0";
+}
+    
